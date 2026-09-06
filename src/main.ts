d036f9ca847ac97b96f1ec5d69c6cfde9ba0e326
hieu92264 <hieu92264@st.vimaru.edu.vn>
2026-09-06 10:55:22 +0700

    feat: enhance styling and theming with Tailwind CSS and custom variables
    - Added Google Fonts import for 'Inter' and integrated tw-animate-css.
    - Defined custom CSS variables for light and dark themes in main.css.
    - Implemented a new Pinia setup in pinia.config.ts with persisted state.
    - Created a reusable Button component with variants and utility functions.
    - Updated TypeScript configuration for improved path mapping and compiler options.
    - Introduced a utility function for class name merging using clsx and tailwind-merge.
    - Added components.json for component configuration and schema validation.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import './assets/styles/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { setupPinia } from '@/configs/pinia.config.ts'
 
 const app = createApp(App)
-
-app.use(createPinia())
 app.use(router)
+
+setupPinia(app)
 
 app.mount('#app')
