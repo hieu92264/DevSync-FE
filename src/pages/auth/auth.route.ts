14554da7a6c30413013cefe0affe816969ceb61c
hieu92264 <hieu92264@st.vimaru.edu.vn>
2026-09-06 11:14:59 +0700

    feat: add authentication routing and related components

---
+++ src/pages/auth/auth.route.ts
@@ -0,0 +1,3 @@
+import type { RouteRecord } from 'vue-router'
+
+export const authRoute: RouteRecord[] = []
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
+import { authRoute } from '@/pages/auth/auth.route'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [...authRoute],
 })
 
 export default router
